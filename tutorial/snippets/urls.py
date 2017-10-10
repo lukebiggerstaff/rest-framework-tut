7af275c31c9c326bb3e9425a09f7b28425071e60
luke biggerstaff <luke.biggerstaff@gmail.com>
2017-10-10 12:18:18 -0500

    changed snippets.views to class based views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-app_name = 'snippets'
-
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
