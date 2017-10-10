957c396adda09343a59f0f40f0e901b1000db1be
luke biggerstaff <luke.biggerstaff@gmail.com>
2017-10-10 13:45:46 -0500

    added permissions to snippets, read only for everyone, only users can create new snippets and only that user an update their snippet.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,18 +1,37 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snipet.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
